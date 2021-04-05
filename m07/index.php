3bfaf65764ef1c2a5352b19bf18384cb88f89609
Yuki218 <liuyixing1018@gmail.com>
2021-04-05 10:17:43 -0700

    m07 assignment

---
+++ m07/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Biker store</title>
+
+  <? include "parts/meta.php" ?>
+</head>
+<body>
+  
+  <? include "parts/navbar.php" ?>
+
+    <div class="view-window hero" style="background-image: url(img/Background-img-1.jpg">
+      <h1>Biker style</h1>
+    </div>
+    
+
+    <div class="container pad" id="introduction">
+      <div class="card card-soft card-light card-flat">
+        <div class="grid">
+          <div class="col-xs-12 col-md-6 center-child">
+            <div style="max-width: 75%;">
+              <h2>The biker style</h2>
+              <p>It can be argued that the single most important piece of safety <span>gear when riding a motorcycle is the helmet. In 2015 alone, motorcycle helmets saved an estimated 1,772 motorcyclist lives, and an additional 740 <span>lives could have been saved had the motorcyclist been wearing a helmet when they had an </span>accident. Your state may or may not require that you wear a helmet by law, but you <span>should ALWAYS ride with a helmet. Embrace ATGATT as a riding mindset. All the gear, all the time. Nothing less.</span></p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <img src="http://via.placeholder.com/400/3a3b3c/fff/?text=Helmet0" alt="Helmet0" class="image-cover">
+            <!--http://homedepot.com-->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr class="spacer large">
+
+    <div class="container pad" id="categories">
+      <h2>Helmet1</h2>
+      <div class="grid gap xs-small md-large">
+        <div class="col-xs-6 col-md-3">
+          <figure>
+            <div class="image-circle">
+              <img src="https://via.placeholder.com/400/3a3b3c/fff/?text=Helmet2" alt="Helmet2">
+            </div>
+            <figcation>Helmet2</figcation>
+          </figure>
+        </div>
+        <div class="col-xs-6 col-md-3">
+          <figure>
+            <div class="image-circle">
+              <img src="https://via.placeholder.com/400/3a3b3c/fff/?text=Helmet3" alt="Helmet3">
+            </div>
+            <figcation>Helmet3</figcation>
+          </figure>
+        </div>
+        <div class="col-xs-6 col-md-3">
+          <figure>
+            <div class="image-circle">
+              <img src="https://via.placeholder.com/400/3a3b3c/fff/?text=Helmet4" alt="Helmet4">
+            </div>
+            <figcation>Helmet4</figcation>
+          </figure>
+        </div>
+        <div class="col-xs-6 col-md-3">
+          <figure>
+            <div class="image-circle">
+              <img src="https://via.placeholder.com/400/3a3b3c/fff/?text=Helmet5" alt="Helmet5">
+            </div>
+            <figcation>Helmet4</figcation>
+          </figure>
+      </div>
+    </div>
+
+    <hr class="spacer large">
+
+</body>
+</html>
+
+
+
+
+
+
